ac9a507da38ac85800f42ecd3e9c5a9048cc5ad4
dinildamsith <dinildamsith52@gmail.com>
2024-03-09 22:08:26 +0530

    added context register configuration  classes

--- src/main/java/lk/ijse/d_zone_backend/webAppInitializer.java
+++ src/main/java/lk/ijse/d_zone_backend/webAppInitializer.java
@@ -1,20 +1,22 @@
 package lk.ijse.d_zone_backend;
 
+import lk.ijse.d_zone_backend.config.WebAppConfig;
+import lk.ijse.d_zone_backend.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class webAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
-    @Override
+    @Override // Dispatch Servlet Initialization
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
     }
 }
