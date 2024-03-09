2bd6cbd7bd0bdd7a6aa857a5b5fb409063944429
dinildamsith <dinildamsith52@gmail.com>
2024-03-09 21:58:10 +0530

    added Configuration Classes

---
+++ src/main/java/lk/ijse/d_zone_backend/config/WebAppConfig.java
@@ -0,0 +1,11 @@
+package lk.ijse.d_zone_backend.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "lk.ijse.d_zone_backend")
+public class WebAppConfig {
+}
