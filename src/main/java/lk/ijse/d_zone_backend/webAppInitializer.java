07ad63fa0e2a4e8a427fee0ac4a12883ea0b5ed6
dinildamsith <dinildamsith52@gmail.com>
2024-03-09 21:57:33 +0530

    webAppInitializer class extend AbstractAnnotationConfigDispatcherServletInitializer interface

--- src/main/java/lk/ijse/d_zone_backend/webAppInitializer.java
+++ src/main/java/lk/ijse/d_zone_backend/webAppInitializer.java
@@ -1,4 +1,20 @@
 package lk.ijse.d_zone_backend;
 
-public class webAppInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class webAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[0];
+    }
 }
